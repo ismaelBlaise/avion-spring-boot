f1bdd4a58977a10c5d6196c90811cc3d5ba00201
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-08-12 00:37:36 +0300

    valider

---
+++ src/main/java/com/s6/avion/controller/AuthController.java
@@ -0,0 +1,60 @@
+package com.s6.avion.controller;
+
+import com.s6.avion.model.Utilisateur;
+import com.s6.avion.service.UtilisateurService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UtilisateurService utilisateurService;
+
+    
+    @GetMapping("/")
+    public ModelAndView showLoginForm() {
+        return new ModelAndView("index"); 
+    }
+
+    
+    @PostMapping("/login")
+    public ModelAndView processLogin(@RequestParam String email,
+                                     @RequestParam String mdp,HttpSession session) {
+        ModelAndView mv = new ModelAndView();
+
+        try {
+            Utilisateur utilisateur = utilisateurService.login(email, mdp);
+            
+
+            if(utilisateur.getRole() == null) {
+                throw new RuntimeException("Rôle de l'utilisateur non défini");
+            }
+            if(utilisateur.getRole().getRole().equalsIgnoreCase("admin")) {
+                mv.setViewName("template-back");
+            } else  {
+                mv.setViewName("template-front");
+            } 
+            mv.addObject("page", "default");
+            mv.addObject("utilisateur", utilisateur);
+            session.setAttribute("utilisateur", utilisateur.getIdUtilisateur()); 
+        } catch (RuntimeException e) {
+            mv.setViewName("index"); 
+            mv.addObject("erreur", e.getMessage());
+            mv.addObject("email", email); 
+        }
+
+        return mv;
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/"; 
+    }
+}
---
+++ src/main/java/com/s6/avion/repository/StatutRepository.java
@@ -0,0 +1,7 @@
+package com.s6.avion.repository;
+
+import com.s6.avion.model.Statut;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatutRepository extends JpaRepository<Statut, Integer> {
+}
